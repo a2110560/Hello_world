f3dc15ef453f6cb460a00993ece4f1ef88b81a66
a2110560 <a2110560@gmail.com>
2020-08-15 12:26:02 +0800

    新增hashset

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.HashSet;
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+    }
+
+    public void create(View view) {
+        HashSet<Integer> set=new HashSet<>();//不會重複的函數
+
+        while(set.size()<6){
+            set.add((int)(Math.random()*49+1));//49為數字個數 1為開頭是甚麼數字
+        }
+
     }
 }
